caacc9c9b4e9096f6c81d1b74eeff72db4918f7e
Eduard Gamonal <eduard.gamonal@est.fib.upc.edu>
2013-11-25 20:57:18 +0100

    improves a lot the design chapter -- transformation into html

---
+++ src/from-xml-to-html-properties.js
@@ -0,0 +1,28 @@
+properties:  {
+    ca:  {
+        // x,y,w,h and clickchain same as default language
+        caption: 'ignici&oacute;!'
+    }
+    en:  {
+        x: 124
+        y: 123
+        width: 100
+        height: 100
+        clickchain:  {
+            goto:  {
+                uri: 'details-view'
+            }
+        }
+        caption: 'ignite!'
+    }
+    fr:  {
+        // x,y,w,h and clickchain same as default language
+        caption: 'allez!'
+    }
+}
+classes:  {
+    ui:
+        [ 'fl-base-button', 'fl-button' ]
+    style:
+        [  ]
+}
